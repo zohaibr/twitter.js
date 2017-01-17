adf07ebde346e498489e243cd3395b591068dc34
Zohaib Rahman <zohaibr@gmail.com>
2017-01-17 11:19:56 -0500

    DONE NUNJUCKS SON

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const volleyball = require('volleyball');
+const nunjucks = require('nunjucks');
 const app = express();
 
 app.use(volleyball);
 
-app.get('/', function (req, res){
-  res.send('server listening');
+//We are defining what to render with nunjucks with the below object
+const locals = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+//This is the boiler setup for Nunjucks
+//App.set is setting the default view engine's extension to HTML.
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', {noCache: true});
+
+//Below renders to index.html in the views folder.
+nunjucks.render('index.html', locals, function (err, output) {
+  if (err) throw err;
+  console.log(output);
 });
 
-app.listen(3000);
+
+app.get('/views', function (req, res){
+  res.render('index', locals, function(err, html) {
+    if (err) console.log(err);
+    res.send(html);
+  });
+});
+
+app.listen(3001);
